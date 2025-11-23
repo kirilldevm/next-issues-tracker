2e562e13d5510c34ee3484c2ce9c7cdb0ad9095e
kirilldev <kirill.makliuk.dev@gmail.com>
2025-11-24 01:10:18 +0200

    feat: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,29 @@
 import { PAGES } from '@/configs/pages.config';
 import Link from 'next/link';
+import { AiFillBug } from 'react-icons/ai';
+
+const links = [
+  { name: 'Home', href: PAGES.HOME },
+  { name: 'Dashboard', href: PAGES.DASHBOARD },
+];
 
 export default function Navbar() {
   return (
     <nav className='flex flex-row gap-4 justify-between border-b border-zinc-200 p-4 mb-5'>
-      <Link href={PAGES.HOME}>Logo</Link>
+      <Link href={PAGES.HOME}>
+        <AiFillBug size={32} />
+      </Link>
       <ul className='flex gap-2'>
-        <li>
-          <Link href={PAGES.DASHBOARD}>Dashboard</Link>
-        </li>
-        <Link href={PAGES.DASHBOARD}>Dashboard</Link>
-        <li></li>
-        <li></li>
+        {links.map((link) => (
+          <li key={link.name}>
+            <Link
+              className='text-zinc-500 hover:text-zinc-800 transition-colors'
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
